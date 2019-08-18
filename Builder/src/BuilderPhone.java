fc3898be621e6dc9e9efe33acec54cd03d762de3
mukesh kumar <mukumar@vlocity.com>
2019-08-19 00:35:56 +0530

    second project

---
+++ Builder/src/BuilderPhone.java
@@ -0,0 +1,38 @@
+
+public class BuilderPhone {
+	
+	private String OS;
+	private int ram;
+	private String processor;
+	private double screenSize;
+	private int battery;
+	
+	public BuilderPhone setOS(String oS) {
+		OS = oS;
+		return this;
+	}
+	public BuilderPhone setRam(int ram) {
+		this.ram = ram;
+		return this;
+	}
+	public BuilderPhone setProcessor(String processor) {
+		this.processor = processor;
+		return this;
+	}
+	public BuilderPhone setScreenSize(double screenSize) {
+		this.screenSize = screenSize;
+		return this;
+	}
+	public BuilderPhone setBattery(int battery) {
+		this.battery = battery;
+		return this;
+	}
+	
+	
+	public Phone getPhone()
+	{
+		return new Phone(OS, ram, processor, screenSize, battery);
+	}
+	
+
+}
